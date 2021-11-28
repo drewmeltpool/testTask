a6ae9aec78d87cb0080538913b93fd333711206f
drewmeltpool <drewmelt132@gmail.com>
2021-11-28 20:24:45 +0300

    fix: post--m scss

--- src/hooks/useWeather/index.tsx
+++ src/hooks/useWeather/index.tsx
@@ -13,7 +13,7 @@ const useWeather = ({ city, units }: WeatherType) => {
     (async () => {
       try {
         const data = await fetch(
-          `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&APPID=7d7d2998ae5b1aa6117b08a5c354b08c`
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&APPID=7d7d2998ae5b1aa6117b08a5c354b08c`
         ).then((d) => d.json());
         setWeather(data);
       } catch (error) {
